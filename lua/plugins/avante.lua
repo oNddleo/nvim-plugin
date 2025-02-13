a5b88d00cd818bd78aad4461819e412bbefcbe1c
LongND <nguyenduclong12a2nd@hotmail.com>
2025-02-13 22:46:33 +0700

    fix: add avante deepseek

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,121 +1,186 @@
 -- https://github.com/yetone/avante.nvim
-return  {
-  "yetone/avante.nvim",
-  event = "VeryLazy",
-  lazy = false,
-  version = false, -- set this if you want to always pull the latest change
-  opts = {
-    -- add any opts here
-  },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  build = "make",
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "stevearc/dressing.nvim",
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    --- The below dependencies are optional,
-    "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    "zbirenbaum/copilot.lua", -- for providers='copilot'
-    {
-      -- support for image pasting
-      "HakonHarnes/img-clip.nvim",
-      event = "VeryLazy",
-      opts = {
-        -- recommended settings
-        default = {
-          embed_image_as_base64 = false,
-          prompt_for_file_name = false,
-          drag_and_drop = {
-            insert_mode = true,
-          },
-          -- required for Windows users
-          use_absolute_path = true,
+-- return  {
+--   "yetone/avante.nvim",
+--   event = "VeryLazy",
+--   lazy = false,
+--   version = false, -- set this if you want to always pull the latest change
+--   opts = {
+--     -- add any opts here
+--       provider = "deepseek",
+--       vendors = {
+--         deepseek = {
+--           __inherited_from = "openai",
+--           api_key_name = "DEEPSEEK_API_KEY",
+--           endpoint = "https://api.deepseek.com",
+--           model = "deepseek-coder",
+--         },
+--       },
+--   },
+--   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+--   build = "make",
+--   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+--   dependencies = {
+--     "nvim-treesitter/nvim-treesitter",
+--     "stevearc/dressing.nvim",
+--     "nvim-lua/plenary.nvim",
+--     "MunifTanjim/nui.nvim",
+--     --- The below dependencies are optional,
+--     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+--     "zbirenbaum/copilot.lua", -- for providers='copilot'
+--     {
+--       -- support for image pasting
+--       "HakonHarnes/img-clip.nvim",
+--       event = "VeryLazy",
+--       opts = {
+--         -- recommended settings
+--         default = {
+--           embed_image_as_base64 = false,
+--           prompt_for_file_name = false,
+--           drag_and_drop = {
+--             insert_mode = true,
+--           },
+--           -- required for Windows users
+--           use_absolute_path = true,
+--         },
+--       },
+--     },
+--     {
+--       -- Make sure to set this up properly if you have lazy=true
+--       'MeanderingProgrammer/render-markdown.nvim',
+--       opts = {
+--         file_types = { "markdown", "Avante" },
+--       },
+--       ft = { "markdown", "Avante" },
+--     },
+--   },
+--   config = function ()
+--     require('avante_lib').load()
+-- require('avante').setup({
+--   -- provider = "claude", -- Recommend using Claude
+--   -- auto_suggestions_provider = "claude", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
+--   -- claude = {
+--   --   endpoint = "https://api.anthropic.com",
+--   --   model = "claude-3.5-sonnet-20241022",
+--   --   temperature = 0,
+--   --   max_tokens = 4096,
+--   -- },
+--   behaviour = {
+--     auto_suggestions = false, -- Experimental stage
+--     auto_set_highlight_group = true,
+--     auto_set_keymaps = true,
+--     auto_apply_diff_after_generation = false,
+--     support_paste_from_clipboard = false,
+--   },
+--   mappings = {
+--     --- @class AvanteConflictMappings
+--     diff = {
+--       ours = "co",
+--       theirs = "ct",
+--       all_theirs = "ca",
+--       both = "cb",
+--       cursor = "cc",
+--       next = "]x",
+--       prev = "[x",
+--     },
+--     suggestion = {
+--       accept = "<M-l>",
+--       next = "<M-]>",
+--       prev = "<M-[>",
+--       dismiss = "<C-]>",
+--     },
+--     jump = {
+--       next = "]]",
+--       prev = "[[",
+--     },
+--     submit = {
+--       normal = "<CR>",
+--       insert = "<C-s>",
+--     },
+--     sidebar = {
+--       switch_windows = "<Tab>",
+--       reverse_switch_windows = "<S-Tab>",
+--     },
+--   },
+--   hints = { enabled = true },
+--   windows = {
+--     ---@type "right" | "left" | "top" | "bottom"
+--     position = "right", -- the position of the sidebar
+--     wrap = true, -- similar to vim.o.wrap
+--     width = 50, -- default % based on available width
+--     sidebar_header = {
+--       align = "center", -- left, center, right for title
+--       rounded = true,
+--     },
+--   },
+--   highlights = {
+--     ---@type AvanteConflictHighlights
+--     diff = {
+--       current = "DiffText",
+--       incoming = "DiffAdd",
+--     },
+--   },
+--   --- @class AvanteConflictUserConfig
+--   diff = {
+--     autojump = true,
+--     ---@type string | fun(): any
+--     list_opener = "copen",
+--   },
+-- })
+--   end
+-- }
+return {
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false, -- set this if you want to always pull the latest change
+    opts = {
+      provider = "deepseek",
+      vendors = {
+        deepseek = {
+          __inherited_from = "openai",
+          api_key_name = "DEEPSEEK_API_KEY",
+          endpoint = "https://api.deepseek.com",
+          model = "deepseek-coder",
         },
       },
     },
-    {
-      -- Make sure to set this up properly if you have lazy=true
-      'MeanderingProgrammer/render-markdown.nvim',
-      opts = {
-        file_types = { "markdown", "Avante" },
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    build = "make",
+    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
       },
-      ft = { "markdown", "Avante" },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
     },
   },
-  config = function ()
-    require('avante_lib').load()
-require('avante').setup({
-  provider = "claude", -- Recommend using Claude
-  auto_suggestions_provider = "claude", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
-  claude = {
-    endpoint = "https://api.anthropic.com",
-    model = "claude-3-haiku",
-    temperature = 0,
-    max_tokens = 4096,
-  },
-  behaviour = {
-    auto_suggestions = false, -- Experimental stage
-    auto_set_highlight_group = true,
-    auto_set_keymaps = true,
-    auto_apply_diff_after_generation = false,
-    support_paste_from_clipboard = false,
-  },
-  mappings = {
-    --- @class AvanteConflictMappings
-    diff = {
-      ours = "co",
-      theirs = "ct",
-      all_theirs = "ca",
-      both = "cb",
-      cursor = "cc",
-      next = "]x",
-      prev = "[x",
-    },
-    suggestion = {
-      accept = "<M-l>",
-      next = "<M-]>",
-      prev = "<M-[>",
-      dismiss = "<C-]>",
-    },
-    jump = {
-      next = "]]",
-      prev = "[[",
-    },
-    submit = {
-      normal = "<CR>",
-      insert = "<C-s>",
-    },
-    sidebar = {
-      switch_windows = "<Tab>",
-      reverse_switch_windows = "<S-Tab>",
-    },
-  },
-  hints = { enabled = true },
-  windows = {
-    ---@type "right" | "left" | "top" | "bottom"
-    position = "right", -- the position of the sidebar
-    wrap = true, -- similar to vim.o.wrap
-    width = 50, -- default % based on available width
-    sidebar_header = {
-      align = "center", -- left, center, right for title
-      rounded = true,
-    },
-  },
-  highlights = {
-    ---@type AvanteConflictHighlights
-    diff = {
-      current = "DiffText",
-      incoming = "DiffAdd",
-    },
-  },
-  --- @class AvanteConflictUserConfig
-  diff = {
-    autojump = true,
-    ---@type string | fun(): any
-    list_opener = "copen",
-  },
-})
-  end
 }
